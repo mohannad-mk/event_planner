e7131c778e54ce26c323aa4786eaa80d2ee3972f
mohannad-mk <mohannad.mksb23@gmail.com>
2025-10-19 03:01:42 +0300

    new sections

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
-import './EventPlanner.css'; // Import CSS file for styling
+import React from "react";
+import "./EventPlanner.css"; // Import CSS file for styling
 
 const EventPlanner = () => {
-    return (
-        <div className="event-planner-container">
-            <header>
-                <h1>Welcome to Event Planner</h1>
-            </header>
-        </div>
-    );
+  return (
+    <div className="event-planner-container">
+      <header>
+        <h1>Welcome to Event Planner</h1>
+      </header>
+      <section className="description">
+        <p>
+          Plan and organize your events effortlessly with Event Planner. From
+          birthdays to corporate meetings, we've got you covered.
+        </p>
+        <button className="get-started-button">Get Started</button>
+      </section>
+      <section className="events_categories">
+        <ul>
+          <h2>Social Events:</h2>
+          <li>Birthday parties</li>
+          <li>Anniversary celebrations</li>
+          <li>Wedding receptions</li>
+          <li>Baby showers</li>
+          <li>Graduation parties</li>
+          <li>Family reunions</li>
+        </ul>
+      </section>
+      <section className="features"></section>
+      <section className="testimonials"></section>
+      <section className="contact"></section>
+    </div>
+  );
 };
 
 export default EventPlanner;
